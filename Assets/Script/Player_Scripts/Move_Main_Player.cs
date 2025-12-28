d0cf3f9217170546c8a4df1c7ba8307b1c79dda2
Platon <platon12802014@gmail.com>
2025-12-28 17:29:40 +0200

    add script main player

---
+++ Assets/Script/Player_Scripts/Move_Main_Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move_Main_Player : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Keyboard.current.wKey.isPressed)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+        if(Keyboard.current.sKey.isPressed)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 5);
+        }
+        if(Keyboard.current.aKey.isPressed)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 5);
+        }
+        if(Keyboard.current.dKey.isPressed)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 5);
+        }
+    }
+}
