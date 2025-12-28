6d8666618d023801c5b3375dac46409b3eeb95e0
Platon <platon12802014@gmail.com>
2025-12-28 14:42:26 +0200

    add trigger

--- Assets/Script/Trigger_Scripts/Play_Trigger.cs
+++ Assets/Script/Trigger_Scripts/Play_Trigger.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class Play_Trigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+     void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player has entered the trigger area.");
+        }
     }
 }
